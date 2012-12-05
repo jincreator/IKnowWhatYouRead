4ad6f6f7842df46ec8ef5f36d16a6d40cfc7ab07
Jinkyu Yi <jincreator@jincreator.net>
2012-12-05 21:26:47 +0900

    [Crawler]Implement basic connection through URL.

--- src/org/cauoop/crawler/ArticleCrawler.java
+++ src/org/cauoop/crawler/ArticleCrawler.java
@@ -1,5 +1,22 @@
 package org.cauoop.crawler;
 
-public class ArticleCrawler {
+import java.net.*;
+import java.io.*;
 
+public class ArticleCrawler {
+	public void fetch(String address){
+		try {
+			URL cwURL = new URL(address);
+			URLConnection cwURLConnection = cwURL.openConnection();
+			cwURLConnection.connect();
+		} 
+		catch (MalformedURLException e) { 
+			// new URL() failed
+			// ...
+		} 
+		catch (IOException e) {   
+			// openConnection() failed
+			// ...
+		}
+	}
 }
