1cf15af8d69d88448449a5bfe4db482d8fa5403e
Jinkyu Yi <jincreator@jincreator.net>
2012-12-05 22:21:09 +0900

    fetch(String address) method will return page from address.

--- src/org/cauoop/crawler/ArticleCrawler.java
+++ src/org/cauoop/crawler/ArticleCrawler.java
@@ -4,19 +4,35 @@ import java.net.*;
 import java.io.*;
 
 public class ArticleCrawler {
-	public void fetch(String address){
+	public String fetch(String address){
 		try {
 			URL cwURL = new URL(address);
 			URLConnection cwURLConnection = cwURL.openConnection();
 			cwURLConnection.connect();
+			String encoding = cwURLConnection.getContentEncoding();
+			BufferedReader in
+				= new BufferedReader(
+						new InputStreamReader(
+								cwURLConnection.getInputStream(),encoding));
+			//FIXME: Not sure InputStreamReader works well when encoding is null.
+			StringBuilder builder = new StringBuilder();
+			String aux = "";
+			while ((aux = in.readLine()) != null) {
+			    builder.append(aux);
+			}
+			in.close();
+			String page = builder.toString();
+			return page;
 		} 
 		catch (MalformedURLException e) { 
 			// new URL() failed
 			// ...
+			return null;
 		} 
 		catch (IOException e) {   
 			// openConnection() failed
 			// ...
+			return null;
 		}
 	}
 }
