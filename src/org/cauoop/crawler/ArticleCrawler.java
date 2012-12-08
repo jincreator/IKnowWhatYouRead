69f8acc033749f125d2f64eb3c2e4fc443cc2c93
Jinkyu Yi <jincreator@jincreator.net>
2012-12-08 20:00:03 +0900

    Make code simple.

--- src/org/cauoop/crawler/ArticleCrawler.java
+++ src/org/cauoop/crawler/ArticleCrawler.java
@@ -2,14 +2,11 @@ package org.cauoop.crawler;
 
 import java.io.IOException;
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 
 public class ArticleCrawler {
 	public String getHtml(String str) {
-		Document doc;
 		try {
-			doc = Jsoup.connect(str).get();
-			return doc.body().text();
+			return Jsoup.connect(str).get().body().text().replaceAll("\\p{Punct}","");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
