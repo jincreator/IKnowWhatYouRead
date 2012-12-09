1def4bb4d23bcb6311a9ce1eacf369b8a8830fce
Jinkyu Yi <jincreator@jincreator.net>
2012-12-09 17:05:47 +0900

    autoget() will crawl pages of specified category.

--- src/org/cauoop/crawler/ArticleCrawler.java
+++ src/org/cauoop/crawler/ArticleCrawler.java
@@ -55,4 +55,27 @@ public class ArticleCrawler {
 	public String selectGet(String address, String hereText) {
 		return selectGet(address, "", hereText, "");
 	}
+	public String autoGet(String category) {
+		if(category.equalsIgnoreCase("politics")) {
+			String naver_politics = selectGet("http://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=100", ".section_headline", "sectionList.nhn");
+			String daum_politics = selectGet("http://media.daum.net/politics", ".wrap_newsitem", "");
+			return naver_politics + " " + daum_politics;
+		} else if(category.equalsIgnoreCase("economic")) {
+			String naver_economic = selectGet("http://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=101", ".section_headline", "sectionList.nhn");
+			String daum_economic = selectGet("http://media.daum.net/economic", ".wrap_newsitem", "");
+			return naver_economic + daum_economic;
+		} else if(category.equalsIgnoreCase("society")) {
+			String naver_society = selectGet("http://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=102", ".section_headline", "sectionList.nhn");
+			String daum_society = selectGet("http://media.daum.net/society", ".wrap_newsitem", "");
+			return naver_society + " " + daum_society;
+		} else if(category.equalsIgnoreCase("world") || category.equalsIgnoreCase("foreign") || category.equalsIgnoreCase("international")) {
+			String naver_world = selectGet("http://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=104", ".section_headline", "sectionList.nhn");
+			String daum_world = selectGet("http://media.daum.net/foreign", ".wrap_newsitem", "");
+			return naver_world + " " + daum_world;
+		} else if(category.equalsIgnoreCase("entertain")) {
+			String naver_entertain = selectGet("http://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=106", ".section_headline","sectionList.nhn");
+			return naver_entertain;
+		}
+	return null;
+	}
 }
