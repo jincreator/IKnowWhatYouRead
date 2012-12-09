b0621a708034be78fcb58875c2963fb46c61a9f7
Jinkyu Yi <jincreator@jincreator.net>
2012-12-09 16:45:55 +0900

    getHtml() can now parse specified area.

--- src/org/cauoop/crawler/ArticleCrawler.java
+++ src/org/cauoop/crawler/ArticleCrawler.java
@@ -2,15 +2,26 @@ package org.cauoop.crawler;
 
 import java.io.IOException;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 public class ArticleCrawler {
-	public String getHtml(String str) {
+	public String getHtml(String str, String hereText) {
 		try {
-			return Jsoup.connect(str).get().body().text().replaceAll("\\p{Punct}","");
+			Document doc = Jsoup.connect(str).get();
+			String page;
+			if(hereText.isEmpty()) {
+				page = doc.body().text();
+			} else {
+				page = doc.select(hereText).text();
+			}
+			return page.replaceAll("\\p{Punct}","");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return null;
+	}
+	public String getHtml(String str) {
+		return getHtml(str, "");
 	}
 }
